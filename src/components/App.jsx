3ee11cc543ddc467a76c5bf5ddf6307ee2d99a7e
Samuel McKnight <samuellp@gmail.comgit statusgit config --global user.user.email iamsamuellp@gmail.com>
2021-09-07 16:53:15 -0500

    Complete Navigation functionality For Going to Next and Previous Books.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,9 +11,33 @@ class App extends Component {
       {title : "A Dance with Dragons",author:"George R.R. Martin"}
     ];
     this.state = {
-      bookNumber: 1
+      bookNumber: 0
     };
   }
+
+  goToNextBook = () => {
+    let tempBookNumber = this.state.bookNumber;
+    tempBookNumber++;
+    if(tempBookNumber === this.books.length){
+      tempBookNumber = 0;
+    }
+
+    this.setState({
+      bookNumber: tempBookNumber
+    });
+
+  }
+
+  goToPrevious= () => {
+    let tempBookNumber = this.state.bookNumber;
+    tempBookNumber--;
+    if(tempBookNumber< 0){
+      tempBookNumber=this.books.length - 1;
+      }
+      this.setState({
+        bookNumber: tempBookNumber
+      })
+    }
 
   render() {
       return (
@@ -22,14 +46,17 @@ class App extends Component {
           <div className= "row"> 
           <div className="col-md-4">
             {/*Button here to move the previous book viewed */}
+            <button onClick={this.goToPreviousBook}>Previous Book</button>
           </div>
           <div className="col-md-4">
             {/*Display book with cover here  */}
             <h1>{this.books[this.state.bookNumber].title }</h1>
             <h4>{this.books[this.state.bookNumber].author}</h4>
           </div>
-          <div className="col-md-4"></div>
+          <div className="col-md-4">
           {/*Button here to move to the next book viewed */}
+          <button onClick={this.goToNextBook}>Next Book</button>
+          </div>
           </div>
         </div>
       )
